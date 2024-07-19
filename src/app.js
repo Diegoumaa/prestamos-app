1d5124f0d70ae1fa50d93cedacee24dc5274a6fe
DIego Umaña <89361155+Diegoumaa@users.noreply.github.com>
2024-07-18 20:36:19 -0600

    .

--- src/app.js
+++ src/app.js
@@ -23,7 +23,12 @@ mongoose.connect(process.env.MONGO_URI, {
 }).then(() => {
   console.log('Connected to MongoDB');
 }).catch((err) => {
-  console.error(err);
+  console.error('Error connecting to MongoDB:', err);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
